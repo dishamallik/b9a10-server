6595ed3e4d33c6625878cc4c652ba26a8332a14d
dishamallik <dishamallik758@gmail.com>
2024-04-28 20:29:29 +0600

    created simple website

--- index.js
+++ index.js
@@ -33,12 +33,31 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+const artCollection = client.db('artDB').collection('art');
+
+
+
+
+app.post('/art' , async(req, res)=>{
+    const newCraft = req.body;
+    console.log(newCraft);
+    const result = await artCollection.insertOne(newCraft);
+    res.send(result)
+})
+
+
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
