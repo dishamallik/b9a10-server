4381e448ddf2bbe0570f42dba1db660fe1ee4ffc
dishamallik <dishamallik758@gmail.com>
2024-04-28 19:01:27 +0600

    created simple website

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -13,10 +13,12 @@ app.use(express.json());
 
 // artDrawing
 // JeVpu8FLMs5HPm5g
+console.log(process.env.DB_USER);
+console.log(process.env.DB_PASS);
 
 
-
-const uri = "mongodb+srv://artDrawing:JeVpu8FLMs5HPm5g@cluster0.1qpflqd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1qpflqd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+console.log(uri);
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
