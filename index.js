4192e2824080c7e9aa0516cff5c0e1f25ec5e542
dishamallik <dishamallik758@gmail.com>
2024-04-29 04:35:50 +0600

    created simple website

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -41,6 +41,15 @@ app.get('/art', async(req, res) => {
     const result = await cursor.toArray();
     res.send(result);
 })
+
+app.get('/art/:id', async(req, res) => {
+    const id = req.params.id;
+    const query = {_id: new ObjectId(id)}
+    const result = await artCollection.findOne(query);
+    res.send(result);
+})
+
+
 
 
 app.post('/art' , async(req, res)=>{
